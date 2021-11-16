90a3855d1aab8df760395d90eff27a84d22d6f53
jorge torres <jorgetorres9410@hotmail.com>
2021-11-16 16:47:59 -0500

    orden por fecha en el dashboard

--- RDC/wsgi.py
+++ RDC/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RDC.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RDC.settings.local')
 
 #application = get_wsgi_application()
 
