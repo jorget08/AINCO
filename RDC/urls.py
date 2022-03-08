98cddfa7a75d4f64fc8cceec5779bc4bf884dc6e
jorget08 <jorgetorres9410@hotmail.com>
2022-03-08 12:18:21 -0500

    heroku2

--- RDC/urls.py
+++ RDC/urls.py
@@ -19,10 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('', include('applications.users.urls')),
-    #path('', include('applications.gestion.urls')),
-    #path('', include('applications.biblioteca.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('applications.users.urls')),
+    path('', include('applications.gestion.urls')),
+    path('', include('applications.biblioteca.urls')),
     
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
